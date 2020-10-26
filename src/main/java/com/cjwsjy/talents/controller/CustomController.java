2e4307dca19e7768a2c60d36e79d9e6e02e9ceff
kongxu08 <232090914@qq.com>
2020-10-26 12:41:31 +0800

    新增查询

--- src/main/java/com/cjwsjy/talents/controller/CustomController.java
+++ src/main/java/com/cjwsjy/talents/controller/CustomController.java
@@ -31,31 +31,41 @@ public class CustomController {
     @ApiOperation(value = "人员岗级分布")
     @GetMapping("/getLevelNum")
     @ResponseBody
-    ResponseResult getLevelNum() {
-        List list = mapper.custom("SELECT glbdef3 'title',  COUNT ( glbdef3 ) as 'num' " +
+    ResponseResult getLevelNum(@RequestBody Map body) {
+        String param = "";
+        //单位
+        if (body.get("org") != null) {
+            param+=String.format(" and analysisorg1='%s'",body.get("org"));
+        }
+        List list = mapper.custom(String.format("SELECT glbdef3 'title',  COUNT ( glbdef3 ) as 'num' " +
                 "FROM   hr_dm_num_cube  " +
                 "WHERE " +
-                " glbdef3 IS NOT NULL  " +
+                " glbdef3 IS NOT NULL %s " +
                 "GROUP BY " +
                 " glbdef3 " +
-                " ORDER BY COUNT ( glbdef3 ) desc");
+                " ORDER BY COUNT ( glbdef3 ) desc",param));
         return ResponseResult.success(list, null);
     }
 
     @ApiOperation(value = "人员专业技术职务")
     @GetMapping("/getPostNum")
     @ResponseBody
-    ResponseResult getPostNum() {
-        List list = mapper.custom("SELECT " +
+    ResponseResult getPostNum(@RequestBody Map body) {
+        String param = "";
+        //单位
+        if (body.get("org") != null) {
+            param+=String.format(" and analysisorg1='%s'",body.get("org"));
+        }
+        List list = mapper.custom(String.format("SELECT " +
                 " glbdef23 'title', " +
                 " COUNT ( glbdef3 ) as 'num' " +
                 "FROM " +
                 " hr_dm_num_cube  " +
                 "WHERE " +
-                " glbdef23 IS NOT NULL  " +
+                " glbdef23 IS NOT NULL %s " +
                 "GROUP BY " +
                 " glbdef23 " +
-                " ORDER BY COUNT ( glbdef23 ) desc");
+                " ORDER BY COUNT ( glbdef23 ) desc",param));
         return ResponseResult.success(list, null);
     }
 
@@ -87,11 +97,18 @@ public class CustomController {
     ResponseResult getLDBZPage(@RequestBody Map body) {
         int current = Integer.parseInt(body.get("current").toString());
         int pagesize = Integer.parseInt(body.get("pagesize").toString());
-        String age_range = body.get("ages").toString();
+
         IPage<Dm_num_cube> page = new Page<Dm_num_cube>(current, pagesize);
         QueryWrapper<Dm_num_cube> wrapper = new QueryWrapper<Dm_num_cube>();
         String sql = "SELECT DISTINCT pk_psndoc from hr_dm_xzzw_post WHERE glbdef12 in ('副处级','正处级','副局级','正局级') AND glbdef11 <> '让岗'";
+        //年龄段
+        String age_range = body.get("ages").toString();
         wrapper.like("age_range", age_range);
+        //单位
+        if (body.get("org") != null) {
+           String org=body.get("org").toString();
+           wrapper.eq("analysisorg1",org);
+        }
         wrapper.inSql("pk_psndoc",sql);
         wrapper.ne("glrc","");
         wrapper.orderByAsc("CORPSEQ","DEPTSEQ","SHOWORDER");
@@ -148,41 +165,103 @@ public class CustomController {
     @ApiOperation(value = "获得称号人才数量")
     @GetMapping("/getHighTalentNum")
     @ResponseBody
-    ResponseResult getCHNum() {
-        List list = mapper.custom("select glbdef8name 'title',count(glbdef8name) 'num'\n" +
-                "from HR_DM_HI_PSNDOC_GLBDEF11\n" +
-                "where glbdef8name is not null\n" +
-                "GROUP BY glbdef8name,glbdef8code\n" +
-                "ORDER BY glbdef8code");
+    ResponseResult getCHNum(@RequestBody Map body) {
+        String param = "";
+        //单位
+        if (body.get("org") != null) {
+            param+=String.format(" and b.analysisorg1='%s'",body.get("org"));
+        }
+        List list = mapper.custom(String.format(" select glbdef8name 'title',count(glbdef8name) 'num' " +
+                " from HR_DM_HI_PSNDOC_GLBDEF11 a,hr_dm_num_cube b " +
+                " where glbdef8name is not null and a.pk_psndoc=b.pk_psndoc " +
+                " GROUP BY glbdef8name,glbdef8code " +
+                " ORDER BY glbdef8code ",param));
         return ResponseResult.success(list, null);
     }
 
     @ApiOperation(value = "执（职）业资格（含三维设计证书）")
     @GetMapping("/getZYZGNum")
     @ResponseBody
-    ResponseResult getZYZGNum() {
-        List list = mapper.custom("select a.glbdef2 'title',count(a.glbdef2) 'num'\n" +
-                "from hr_dw_zyzg a,HR_DIM_ZYZG b\n" +
-                "where a.glbdef2=b.name and a.glbdef2 is not null and a.pk_psndoc is not null\n" +
-                "GROUP BY a.glbdef2,b.code\n" +
-                "ORDER BY b.code");
+    ResponseResult getZYZGNum(@RequestBody Map body) {
+        String param = "";
+        //单位
+        if (body.get("org") != null) {
+            param+=String.format(" and c.analysisorg1='%s'",body.get("org"));
+        }
+        List list = mapper.custom(String.format(" select a.glbdef2 'title',count(a.glbdef2) 'num' " +
+                " from hr_dw_zyzg a,HR_DIM_ZYZG b,hr_dm_num_cube c " +
+                " where a.glbdef2=b.name and c.pk_psndoc=a.pk_psndoc and a.glbdef2 is not null and a.pk_psndoc is not null %s " +
+                " GROUP BY a.glbdef2,b.code " +
+                " ORDER BY b.code",param));
         return ResponseResult.success(list, null);
     }
 
-    /*
-    select t.glbdef25,sum(t.num) 'num' from (
-            select sex,age,glbdef25,count(glbdef25) 'num'
-    from hr_dm_num_cube_tx a
-    GROUP BY sex,glbdef25,age
-    HAVING glbdef25 in('技术工人','普通工人','工人') and ((sex='男' and age>60) or (sex='女' and age>50 and glbdef25='工人') or (sex='女' and age>55 and glbdef25!='工人'))
-            ) t
-    GROUP BY glbdef25
-    */
     @ApiOperation(value = "近五年退休人员及趋势")
     @GetMapping("/getTXRYQSNum")
     @ResponseBody
-    ResponseResult getTXRYQSNum() {
-        List list = mapper.custom("");
+    ResponseResult getTXRYQSNum(@RequestBody Map body) {
+        String param = "";
+        //单位
+        if (body.get("org") != null) {
+            param+=String.format(" and a.analysisorg1='%s'",body.get("org"));
+        }
+        List list = mapper.custom(String.format(" select t.glbdef25,sum(t.num) 'num' from ( " +
+                " select sex,age,glbdef25,count(glbdef25) 'num' " +
+                " from hr_dm_num_cube_tx a where 1=1 %s " +
+                " GROUP BY sex,glbdef25,age " +
+                " HAVING glbdef25 in('技术工人','普通工人','工人') and ((sex='男' and age>60) or (sex='女' and age>50 and glbdef25='工人') or (sex='女' and age>55 and glbdef25!='工人')) " +
+                " ) t " +
+                " GROUP BY glbdef25",param));
         return ResponseResult.success(list, null);
+    }
+
+    @ApiOperation(value = "近五年退休人员及趋势分页查询")
+    @GetMapping("/getTXRYQSPage")
+    @ResponseBody
+    ResponseResult getTXRYQSPage(@RequestBody Map body) {
+        int current = Integer.parseInt(body.get("current").toString());
+        int pagesize = Integer.parseInt(body.get("pagesize").toString());
+
+        IPage<Dm_num_cube> page = new Page<Dm_num_cube>(current, pagesize);
+        QueryWrapper<Dm_num_cube> wrapper = new QueryWrapper<Dm_num_cube>();
+        String param = "";
+        //单位
+        if (body.get("org") != null) {
+            param+=String.format(" and a.analysisorg1='%s'",body.get("org"));
+        }
+        //工种glbdef25
+        String glbdef25 = body.get("glbdef25").toString();
+        List list = mapper.custom(String.format("SELECT " +
+                " pk_psndoc  " +
+                " FROM " +
+                " hr_dm_num_cube_tx a  " +
+                " WHERE " +
+                " 1 = 1 AND glbdef25 IN ( %s )  " +
+                " %s AND ( " +
+                " ( sex = '男' AND age > 60 )  " +
+                " OR ( sex = '女' AND age > 50 AND glbdef25 = '工人' )  " +
+                " OR ( sex = '女' AND age > 55 AND glbdef25 != '工人' ) ",glbdef25,param));
+        return ResponseResult.success(list, null);
+    }
+
+    @ApiOperation(value = "交流挂职人员分页查询")
+    @GetMapping("/getJLGZPage")
+    @ResponseBody
+    ResponseResult getJLGZPage(@RequestBody Map body) {
+        int current = Integer.parseInt(body.get("current").toString());
+        int pagesize = Integer.parseInt(body.get("pagesize").toString());
+
+        IPage<Dm_num_cube> page = new Page<Dm_num_cube>(current, pagesize);
+        QueryWrapper<Dm_num_cube> wrapper = new QueryWrapper<Dm_num_cube>();
+        String sql = "select pk_psndoc from hr_dw_num_jlgz";
+        //单位
+        if (body.get("org") != null) {
+            String org=body.get("org").toString();
+            wrapper.eq("analysisorg1",org);
+        }
+        wrapper.inSql("pk_psndoc",sql);
+        wrapper.orderByAsc("CORPSEQ","DEPTSEQ","SHOWORDER");
+        IPage result = dmNumCubeService.page(page, wrapper);
+        return ResponseResult.success(result, null);
     }
 }
